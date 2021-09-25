2b9d4722fce8b1112fdc105ec3b34fd451a4f2a1
Buknoy <66095051+madgarcia@users.noreply.github.com>
2021-09-25 20:32:16 +1000

    Delete ITami.sol
    duplicate

--- ITami.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.2;
-
-interface ITami {
-
-      function isExcludedFromFee(address account) external view returns(bool);
-
-      function owner() external view returns(address);
-}
